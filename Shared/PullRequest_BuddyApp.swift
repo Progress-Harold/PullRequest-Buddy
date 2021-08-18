49c9cdb93f76e169e5877b48fe03acd33b26ac58
Lee Davis <harold_davis@icloud.com>
2021-08-17 23:45:53 -0700

    Initial commit

---
+++ Shared/PullRequest_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullRequest_BuddyApp.swift
+//  Shared
+//
+//  Created by Lee Davis on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct PullRequest_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
